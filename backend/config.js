bf7bdaf197fbb3023efbbfa06ea61587161c3f6f
Axel Torrico <torricohernan2@gmail.com>
2023-04-04 10:50:07 -0300

    comando start añadido

--- backend/config.js
+++ backend/config.js
@@ -3,7 +3,7 @@ export const PORT = process.env.PORT || 3000
 
 //si existe que lea la variable, sino que use la otra
 export const DB_HOST = process.env.DB_HOST || 'localhost'
-export const DB_USER = process.env.DB_HOST || 'root'
-export const DB_PASSWORD = process.env.DB_HOST || 'axel'
+export const DB_USER = process.env.DB_USER || 'root'
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'axel'
 export const DB_PORT = process.env.DB_PORT || 3306
-export const DB_NAME = process.env.DB_HOST || 'pizzamania'
+export const DB_NAME = process.env.DB_NAME || 'pizzamania'
